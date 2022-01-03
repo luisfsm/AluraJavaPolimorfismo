ba800c51c7a2cdd3652cf087da811f3d032bc5cd
luis felipe <luis.s.mendes20@gmail.com>
2022-01-03 19:39:16 -0300

    Projeto Inicial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# AluraJavaPolimorfismo
+# Java Polimorfismo: Entenda herança e interfaces
+
+## Faça esse curso de Java e:
+- Reuse código com classes Herança e Composição
+- Implemente interface e métodos abstratas
+- Entenda o que é Polimorfismo
+- Aprenda sobre reescrita e herança de métodos
+- Conheça super e protected
---
+++ bytebank/src/Funcionario.java
@@ -0,0 +1,33 @@
+public class Funcionario {
+    
+    private String nome;
+    private String cpf;
+    private double salario;
+
+
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+}
